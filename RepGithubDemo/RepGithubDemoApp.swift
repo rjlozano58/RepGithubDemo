7d1d58b7d0bbc2b4e41c6fac74d8ef95552db13c
Rogelio Lozano <rjlozano58@gmail.com>
2023-09-30 11:25:26 -0500

    Initial Commit

---
+++ RepGithubDemo/RepGithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepGithubDemoApp.swift
+//  RepGithubDemo
+//
+//  Created by Rogelio Lozano on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct RepGithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RepGithubDemoUITests/RepGithubDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RepGithubDemoUITestsLaunchTests.swift
+//  RepGithubDemoUITests
+//
+//  Created by Rogelio Lozano on 9/30/23.
+//
+
+import XCTest
+
+final class RepGithubDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
